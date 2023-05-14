d67526c9e79904299352b22cd1b3f737c274cef0
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-14 12:44:12 +0300

    +added: DtoFiles For Mapping

---
+++ TrifoyProject/TrifoyProject.Core/DTOs/PlayerFeaturesDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrifoyProject.Core.DTOs
+{
+    public class PlayerFeaturesDTO:BaseDTO
+    {
+        public string? Role { get; set; }
+        public string? Rank { get; set; }
+        public int PlayedGame { get; set; }
+        public int Win { get; set; }
+        public int Lose { get; set; }
+        public int Goal { get; set; }
+        public int Assist { get; set; }
+        public int CS { get; set; }
+        public int Coin { get; set; }
+    }
+}
