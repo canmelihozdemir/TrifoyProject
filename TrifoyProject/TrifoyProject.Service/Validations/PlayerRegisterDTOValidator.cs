e48ca92f536fb755e7428b1b0acb8f4c6bce5658
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-14 12:55:43 +0300

    +added: PlayerRegisterValidatonRules

---
+++ TrifoyProject/TrifoyProject.Service/Validations/PlayerRegisterDTOValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrifoyProject.Core.DTOs;
+
+namespace TrifoyProject.Service.Validations
+{
+    public class PlayerRegisterDTOValidator:AbstractValidator<PlayerRegisterDTO>
+    {
+        public PlayerRegisterDTOValidator()
+        {
+            RuleFor(x=>x.UserName).NotNull().WithMessage("{PropertyName} alanı null olamaz!").NotEmpty().WithMessage("{PropertyName} alanı boş olamaz!");
+            RuleFor(x => x.Password).NotNull().WithMessage("{PropertyName} alanı null olamaz").NotEmpty().WithMessage("{PropertyName} alanı boş olamaz");
+            RuleFor(x => x.Password!.Length).InclusiveBetween(6, 20).WithMessage("{PropertyName} alanı 6-20 karakter arası olmalıdır");
+        }
+    }
+}
