3c7f21d547fbf7c9e584d2608a56356110ab0fba
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-14 16:06:11 +0300

    changed: IdentityConfigurationsMovedExtensionFile

---
+++ TrifoyProject/TrifoyProject.API/Extensions/StartupExtensions.cs
@@ -0,0 +1,26 @@
+﻿using TrifoyProject.Entity;
+using TrifoyProject.Repository;
+
+namespace TrifoyProject.API.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void AddIdentityWithExtension(this IServiceCollection services)
+        {
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                //username default unique dir
+                options.User.RequireUniqueEmail = false;
+                options.User.AllowedUserNameCharacters = string.Empty;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = false;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(4);
+                options.Lockout.MaxFailedAccessAttempts = 7;
+
+            }).AddEntityFrameworkStores<AppIdentityDbContext>();
+        }
+    }
+}
--- TrifoyProject/TrifoyProject.API/Program.cs
+++ TrifoyProject/TrifoyProject.API/Program.cs
@@ -4,24 +4,21 @@ using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
+using TrifoyProject.API.Extensions;
 using TrifoyProject.API.Middlewares;
 using TrifoyProject.API.Modules;
-using TrifoyProject.Core.Repositories;
-using TrifoyProject.Core.Services;
-using TrifoyProject.Core.UnitOfWorks;
 using TrifoyProject.Entity;
 using TrifoyProject.Repository;
-using TrifoyProject.Repository.Repositories;
-using TrifoyProject.Repository.UnitOfWorks;
 using TrifoyProject.Service.Mapping;
-using TrifoyProject.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 
+
 builder.Services.AddAutoMapper(typeof(MapProfile));
+
 
 builder.Services.AddDbContext<AppIdentityDbContext>(options =>
 {
@@ -29,33 +26,16 @@ builder.Services.AddDbContext<AppIdentityDbContext>(options =>
 });
 
 
-builder.Services.AddIdentity<AppUser, AppRole>(options =>
-{
-    //username default unique dir
-    options.User.RequireUniqueEmail = false;
-    options.User.AllowedUserNameCharacters = string.Empty;
-    options.Password.RequiredLength = 6;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireDigit = false;
-
-}).AddEntityFrameworkStores<AppIdentityDbContext>();
-
-
-
-
-
-
+builder.Services.AddIdentityWithExtension();
 
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(conteinerBuilder=>conteinerBuilder.RegisterModule(new RepositoryServiceModule()));
 
 
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
